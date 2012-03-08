597b8c2d1527e7d152d855009f30b5951aa26206
Bartek Radwan <bartek@sq9mev.info>
2012-03-08 18:49:16 +0100

    liquibase.jar setting, postgis backend

--- liquimigrate/__init__.py
+++ liquimigrate/__init__.py
@@ -1,6 +1,9 @@
 import os
-LIQUIBASE_JAR = os.path.join(os.path.dirname(__file__), 'vendor', 'liquibase.jar')
+from django.conf import settings
+
+LIQUIBASE_JAR = getattr(settings, 'LIQUIBASE_JAR', os.path.join(os.path.dirname(__file__), 'vendor', 'liquibase.jar'))
 LIQUIBASE_DRIVERS = {
+    'postgis': ( 'postgresql', 'org.postgresql.Driver', os.path.join(os.path.dirname(__file__), 'vendor', 'connectors', 'postgresql-jdbc3-8.2.jar')),
     'postgresql_psycopg2': ( 'postgresql', 'org.postgresql.Driver', os.path.join(os.path.dirname(__file__), 'vendor', 'connectors', 'postgresql-jdbc3-8.2.jar')),
     'mysql': ( 'mysql', 'com.mysql.jdbc.Driver', os.path.join(os.path.dirname(__file__), 'vendor', 'connectors', 'mysql-connector-java-5.0.8-bin.jar')),
 }
