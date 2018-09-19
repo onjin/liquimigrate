78448cc358c91d23e5e6c006519b56f219078901
Marcin Nowak <marcin.j.nowak@gmail.com>
2018-09-19 20:38:18 +0200

    add possibility to fake migrations and skip sql errors

--- liquimigrate/management/commands/makechangesets.py
+++ liquimigrate/management/commands/makechangesets.py
@@ -19,6 +19,14 @@ class Command(BaseCommand):
             help='Migrations author (defaults to username)',
         )
         parser.add_argument(
+            '--fake', action='store_true', default=False,
+            help='Create fake changesets (mark migrations ran)',
+        )
+        parser.add_argument(
+            '--skip-errors', action='store_true', default=False,
+            help='Skip SQL creation erros',
+        )
+        parser.add_argument(
             '--database', action='store', dest='database',
             default=DEFAULT_DB_ALIAS,
             help='Nominates a database to synchronize. '
@@ -30,10 +38,12 @@ class Command(BaseCommand):
         connection = connections[db]
         app_names = options['app_label']
         author = options['author'] or os.getlogin()
+        fake = options['fake']
 
-        self.generate_changesets(connection, app_names, author)
+        self.generate_changesets(
+                connection, app_names, author, fake, options['skip_errors'])
 
-    def _validate_app_names(self, loader, app_names, author):
+    def _validate_app_names(self, loader, app_names):
         invalid_apps = []
         for app_name in app_names:
             if app_name not in loader.migrated_apps:
@@ -43,7 +53,9 @@ class Command(BaseCommand):
                     'No migrations present for: %s' % (
                         ', '.join(sorted(invalid_apps))))
 
-    def generate_changesets(self, connection, app_names, author):
+    def generate_changesets(
+            self, connection, app_names, author, fake, skip_errors):
+
         loader = MigrationLoader(connection)
         graph = loader.graph
         if app_names:
@@ -75,12 +87,32 @@ class Command(BaseCommand):
         for app_label, name in to_generate:
             migration = executor.loader.get_migration_by_prefix(
                                                         app_label, name)
+            self.output_transaction = migration.atomic
+
             targets = [(app_label, migration.name)]
             forward_plan = [(executor.loader.graph.nodes[targets[0]], False)]
             backward_plan = [(executor.loader.graph.nodes[targets[0]], True)]
 
-            sql_forward = executor.collect_sql(forward_plan)
-            sql_backward = executor.collect_sql(backward_plan)
+            try:
+                sql_forward = executor.collect_sql(forward_plan)
+            except Exception as ex:
+                if skip_errors:
+                    sql_forward = ['-- skipped due to exception: %s' % ex]
+                else:
+                    raise
+
+            try:
+                sql_backward = executor.collect_sql(backward_plan)
+            except Exception as ex:
+                if skip_errors:
+                    sql_backward = ['-- skipped due to exception: %s' % ex]
+                else:
+                    raise
+
+            changeset_id = u'django-%s-%s' % (app_label, migration.name)
+
+            if fake:
+                changeset_id += '-faked'
 
             forward_version = (
                     'insert into django_migrations (app, name, applied) '
@@ -91,16 +123,18 @@ class Command(BaseCommand):
                     'app=\'%s\' and name=\'%s\';' % (
                             app_label, migration.name))
 
-            print('    <changeSet id="django-%s-%s" author="%s">' % (
-                app_label, migration.name, author))
-            print('        <sql><![CDATA[')
-            print('            '+'\n            '.join(sql_forward))
-            print('        ]]></sql>')
+            print('    <changeSet id="%s" author="%s">' % (
+                                            changeset_id, author))
+            if not fake:
+                print('        <sql><![CDATA[')
+                print('            '+'\n            '.join(sql_forward))
+                print('        ]]></sql>')
             print('        <sql>%s</sql>' % forward_version)
             print('        <rollback>')
-            print('            <sql><![CDATA[')
-            print('            '+'\n            '.join(sql_backward))
-            print('            ]]></sql>')
+            if not fake:
+                print('            <sql><![CDATA[')
+                print('            '+'\n            '.join(sql_backward))
+                print('            ]]></sql>')
             print('            <sql>%s</sql>' % backward_version)
             print('        </rollback>')
             print('    </changeSet>')
